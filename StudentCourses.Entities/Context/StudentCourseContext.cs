3e6128828f8de033c0c448ee634e5eefe13ec207
master2080 <master20802@gmail.com>
2021-02-18 18:26:49 +0200

    model creating

--- StudentCourses.Entities/Context/StudentCourseContext.cs
+++ StudentCourses.Entities/Context/StudentCourseContext.cs
@@ -16,11 +16,30 @@ namespace StudentCourses.Models
         public StudentCourseContext(DbContextOptions<StudentCourseContext> options)
             : base(options)
         {
-            
+
         }
 
         public DbSet<Student> Students { get; set; }
         public DbSet<Group> Groups { get; set; }
         public DbSet<Course> Courses { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            string deanRoleId = Guid.NewGuid().ToString();
+            string deanUserId = Guid.NewGuid().ToString();
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole { Id = deanRoleId, Name = "Deans", ConcurrencyStamp = Guid.NewGuid().ToString(), NormalizedName = "DEANS" },
+                new IdentityRole { Id = Guid.NewGuid().ToString(), Name = "Students", ConcurrencyStamp = Guid.NewGuid().ToString(), NormalizedName = "STUDENTS" },
+                new IdentityRole { Id = Guid.NewGuid().ToString(), Name = "Teachers", ConcurrencyStamp = Guid.NewGuid().ToString(), NormalizedName = "TEACHERS" }
+                );
+
+            builder.Entity<IdentityUser>().HasData(
+                new SiteUser { Id = deanUserId, Email = "dean@email.com", UserName = "dean@email.com", ConcurrencyStamp = Guid.NewGuid().ToString(), SecurityStamp = Guid.NewGuid().ToString() },
+                new SiteUser { Id = Guid.NewGuid().ToString(), Email = "student@email.com", UserName = "student@email.com", ConcurrencyStamp = Guid.NewGuid().ToString(), SecurityStamp = Guid.NewGuid().ToString() },
+                new SiteUser { Id = Guid.NewGuid().ToString(), Email = "teacher@email.com", UserName = "teacher@email.com", ConcurrencyStamp = Guid.NewGuid().ToString(), SecurityStamp = Guid.NewGuid().ToString() }
+                );
+
+            //add each to their own roles
+            base.OnModelCreating(builder);
+        }
     }
 }
